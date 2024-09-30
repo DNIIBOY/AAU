acb2e9382ff05315fb8d4edd087739e7f49ed613
Daniel Nettelfield <daniel.nettelfield@gmail.com>
2024-09-30 16:22:15 +0200

    Do a bit of numpy stuff

---
+++ GrundlæggendeProgrammering/numpyarrays.py
@@ -0,0 +1,21 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def h(x):
+    return 1 / (np.sqrt(2 * np.pi)) * np.exp(-0.5 * x**2)
+
+
+def f(x):
+    return 2*x**2 - 10 * x + 12
+
+
+def main():
+    x = np.linspace(-4, 4, num=41)
+    y = h(x)
+    plt.plot(x, y)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
