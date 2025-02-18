be7a67e335a177bca168c219df579794b4b3d552
Daniel Nettelfield <daniel.nettelfield@gmail.com>
2025-02-18 10:48:41 +0100

    Computernetworks 2

---
+++ ComputerNetworks/module1/leaks/frontpage.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>WulffMorgenthaler</title>
+<base target="_top"/>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<meta name="description" content="" />
+<meta http-equiv="imagetoolbar" content="false" />
+<script type="text/javascript" src="http://wulffmorgenthaler.dk/js/swfobject.js"></script>
+<style type="text/css">
+html, body {
+	margin: 0;
+	padding: 0;
+}
+#wm-banner-box img {
+	border: none;
+}
+#wm-banner-box {
+	background: transparent url('http://wulffmorgenthaler.dk/banners/politiken/images/wm-banner-bg.gif') no-repeat top left;
+	width: 440px;
+	height: 340px;
+	margin: 0;
+	padding: 0;
+	overflow: hidden;
+}
+#wm-logo {width: 214px;height:35px;padding: 14px 0 0 11px;}
+#wm-logo a {width: 214px;height:35px;display:block;}
+#wm-logo a span {display:none;}
+#wm-cartoon {padding:1px 0 0 10px;}
+#wm-cartoon .wm-left {width: 283px;float: left;}
+#wm-cartoon .wm-cartoon-big {width:283px;margin: 0;}
+#wm-cartoon .wm-cartoon-big h2 {margin:0;padding:0px 0 5px 9px;font-family:'Arial Black';color:#fff;font-size:11px;font-weight:bold;text-transform:uppercase;}
+#wm-cartoon .wm-cartoon-big img {border: 1px #7815be solid;width: 281px;height: 97px;}
+#stribers {clear: left;}
+#stribers h2 {margin:0;padding:5px 0 0 0;font-family:'Arial Black';color:#fff;font-size:11px;font-weight:bold;text-transform:uppercase;}
+#wm-cartoon .wm-right {width: 146px;float: left;}
+#wm-cartoon .wm-shop {width:137px;height:148px;padding-left: 5px;}
+.wm-cartoons {clear: both;height: 77px;}
+.wm-cartoons .den_bedste, .wm-cartoons .den_vaerste {width:204px;height:72px;float:left;margin:5px 0 0 0;display: inline}
+#wm-banner-box .wm-cartoons img {border: 1px #f8f8f8 solid;}
+.wm-cartoons .den_vaerste {margin-left: 6px;}
+#wm-beavers {width: 436px;height: 60px;margin: 0;}
+</style>
+	<!--[if gte IE 6]>
+	<style>
+	.wm-cartoons {height: 73px;}
+	.wm-cartoons .den_bedste, .wm-cartoons .den_vaerste {margin-top:1px;}
+	</style>
+	<![endif]-->
+</head>
+<body>
+<div id="wm-banner-box">
+	<div id="wm-logo"><a href="http://wulffmorgenthaler.dk?utm_source=POL&utm_medium=banner&utm_campaign=POL_BAGSIDEN"><span>wulffmorgenthaler</span></a></div>
+	<div id="wm-cartoon">
+		<div class="wm-left">
+			<div class="wm-cartoon-big">
+                <h2>Dagens stribe</h2>
+                <div>
+	                <a href="http://wulffmorgenthaler.dk?utm_source=POL&utm_medium=banner&utm_campaign=POL_BAGSIDEN">
+	                    <img src="http://wulffmorgenthaler.dk/strip/thumb/WM_strip_DK_20100506.jpg" alt="Dagens stribe" />
+	                </a>
+                </div>
+			</div>
+			<div id="stribers">
+				<h2>To du ikke forstår…</h2>
+			</div>
+		</div>
+		<div class="wm-right">
+		    <div class="wm-shop">
+		        <a href="https://shop.wulffmorgenthaler.dk/?utm_source=POL&utm_medium=banner&utm_campaign=POL_BAGSIDEN"><img src="./images/shop_btn.png" alt="WM Shop" /></a>
+		    </div>
+		</div>
+		<div class="wm-cartoons">
+			<div class="den_bedste">
+				<a href="http://wulffmorgenthaler.dk/strip/2007/03/04?utm_source=POL&utm_medium=banner&utm_campaign=POL_BAGSIDEN" target="_top">
+					<img src="http://wulffmorgenthaler.dk/strip/thumb/WM_strip_DK_20070304.jpg" alt="" width="204" height="70" />
+				</a>
+			</div>
+			<div class="den_vaerste">
+				<a href="http://wulffmorgenthaler.dk/strip/2009/01/06?utm_source=POL&utm_medium=banner&utm_campaign=POL_BAGSIDEN" target="_top">
+					<img src="http://wulffmorgenthaler.dk/strip/thumb/WM_strip_DK_20090106.jpg" alt="" width="204" height="70" />
+				</a>
+			</div>
+		</div>
+		<div style="clear:left"></div>
+	</div>
+	<div id="wm-beavers"></div>
+	<script type="text/javascript">
+		swfobject.embedSWF("./swf/beavers.swf", "wm-beavers", "60", "436", "8.0.0", "expressInstall.swf", {}, {wmode: "transparent"}, {});
+	</script>
+</div>
+</body>
+</html>
